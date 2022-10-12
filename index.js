649229c2d7cf9f29e81fa0266186fe735fe4a825
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-12 15:59:47 +0300

    feat(db): connect to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    });
+}
 
 app.listen(8800, () => {
+    connect();
     console.log("Server is running on port 8800");
 });
