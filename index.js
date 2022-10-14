2418280b3e797fa991b58667c6c5051954536abb
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-14 16:12:42 +0300

    feat(index.js): add new routes && middlewares

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 dotenv.config();
 
@@ -13,7 +19,24 @@ const connect = () => {
     });
 }
 
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        message,
+        status
+    });
+})
+
 app.listen(8800, () => {
     connect();
-    console.log("Server is running on port 8800");
+    console.log("Connected to Server");
 });
