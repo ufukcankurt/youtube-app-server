7d2073a7e61d00a969d34dc3a8945323b43b6be9
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-31 22:35:11 +0300

    feat(controller): create crud comment methods

---
+++ controllers/comment.js
@@ -0,0 +1,40 @@
+import { createError } from '../error.js';
+import Comment from '../models/Comment.js';
+import Video from '../models/Video.js';
+
+export const addComment = async (req, res, next) => {
+    const newComment = new Comment({ ...req.body, userId: req.user.id });
+    try {
+        const savedComment = await newComment.save();
+        res.status(200).json(savedComment);
+    } catch (error) {
+        next(error);
+        console.log(error)
+    }
+}
+
+export const deleteComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.id);
+        const video = await Video.findById(req.params.id);
+
+        if (req.user.id === comment.userId || req.user.id === video.userId) {
+            await Comment.findByIdAndDelete(req.params.id);
+            res.status(200).json("Comment has been deleted");
+        } else {
+            return next(createError(403, "You can delete only your comments"));
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getComments = async (req, res, next) => {
+    try {
+        const comments = await Comment.find({ videoId: req.params.videoId });
+        res.status(200).json(comments);
+    } catch (error) {
+        next(error);
+    }
+}
