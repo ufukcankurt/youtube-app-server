8ab261e83b6264df3e7aecef554c75a8ad0fd0c1
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-31 19:47:35 +0300

    feat(routes): add verifyToken method to routes

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,29 @@
 import express from "express";
 import { update, deleteUser, getUser, subscribe, unsubscribe, like, dislike } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 // update user
-router.put("/:id", update);
+router.put("/:id", verifyToken, update);
 
 // delete user
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 // get user
 router.get("/find/:id", getUser);
 
 // subscribe user
-router.put("/sub/:id", subscribe);
+router.put("/sub/:id", verifyToken, subscribe);
 
 // unsubscribe user
-router.put("/unsub/:id", unsubscribe);
+router.put("/unsub/:id", verifyToken, unsubscribe);
 
 // like a video
-router.put("/like/:videoId", like);
+router.put("/like/:videoId", verifyToken, like);
 
 // dislike a video
-router.put("/dislike/:videoId", dislike);
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 
 export default router;
