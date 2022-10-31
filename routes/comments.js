a1a19d6cfdeff63ddc41926b6e80726ef0089f7c
ufukcankurt <ufuk.cnk@gmail.com>
2022-10-31 22:36:00 +0300

    feat(routes): create comment routes

---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.delete("/:id", verifyToken, deleteComment);
+router.get("/:videoId", verifyToken, getComments);
+
+export default router;
